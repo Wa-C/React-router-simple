0ea626e531c0f01eafc383a94f2aa98ebde5d976
Syed Wassi Ul Haque <wassi244255@gmail.com>
2021-03-11 22:19:14 +0600

    Details Page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,14 @@ import { Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { FaYoutube } from '@fortawesome/free-solid-svg-icons'
 import { AiFillFacebook } from "react-icons/fa";
+import {faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
     return (
         <Container className="text-center">
             {/* <FontAwesomeIcon icon={FaYoutube} /> */}
             {/* <AiFillFacebook /> */}
+            <FontAwesomeIcon icon={['fab', faFacebook]} />
         </Container>
     );
 };
--- src/components/TeamDetails.js
+++ src/components/TeamDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import male from './images/male.png'
 import female from './images/female.png'
@@ -9,33 +9,33 @@ const TeamDetails = () => {
     const { idTeam } = useParams();
 
     const [Team, setTeam] = useState([]);
-    
+
 
     useEffect(() => {
         fetch(`https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${idTeam}`)
-        .then(res => res.json())
-        .then(data => setTeam(data.teams[0]))
+            .then(res => res.json())
+            .then(data => setTeam(data.teams[0]))
         // console.log(Team);
-        
-     }, [idTeam])
-      
+
+    }, [idTeam])
+
     return (
         <div className="container my-5">
             <div className="row">
                 <div className="col-md-6">
                     {/* <img className="img-fluid" src={``} alt=""/> */}
                 </div>
-                <div className="col-md-6">
-                <div >
-     {
-         Team?.strGender?.toLowerCase() === "male" ? <img src={male}/> : <img src={female}/> 
-     }
-    </div>
-                <div >
-                    <h3>{Team.strTeam}</h3>
-                    <p>Release Date {Team.intFormedYear}</p>
-                    <p>{Team.strLeague}</p>
-                    <p>Gender : {Team.strGender}</p>
+                <div className="row">
+                    <div className="col-md-6">
+                        {
+                            Team?.strGender?.toLowerCase() === "male" ? <img style={{width: "100%"}} src={male} /> : <img src={female} />
+                        }
+                    </div>
+                    <div className="col-md-6"  >
+                        <h3>{Team.strTeam}</h3>
+                        <p>Release Date {Team.intFormedYear}</p>
+                        <p>{Team.strLeague}</p>
+                        <p>Gender : {Team.strGender}</p>
                     </div>
 
                 </div>
